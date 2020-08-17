d48bdafd92bf96bf245821c24b3f8605d64e35f5
Mauro Crosignani <crosignani.mauro@gmail.com>
2020-08-17 01:17:32 -0300

    change images

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,11 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'gatsby'
 
+import logo from '../assets/images/logo-bg.png'
+
 const Header = (props) => (
     <header id="header" className="alt">
-        <Link to="/" className="logo"><strong>Logo</strong></Link>
+        <Link to="/" className="logo"><img src={logo} height="50px" width="50px" /></Link>
         <nav>
             <a className="menu-link" onClick={props.onToggleMenu} href="javascript:;">Menu</a>
         </nav>
